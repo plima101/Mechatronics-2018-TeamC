093910a315b8867eed771308f7b4746ff6feb98a
plima <plima@andrew.cmu.edu>
2018-03-19 14:41:55 -0400

    Spring Break State Machine Update

--- Arudino/WindowWasher/WindowWasher.ino
+++ Arudino/WindowWasher/WindowWasher.ino
@@ -28,12 +28,168 @@
  
 /** END Header Files*/
 
-void setup() {
-  // put your setup code here, to run once:
+/** BEGIN Debug Section */
+//Uncomment Line below for Debug Serial Printing
+//#define DEBUG
 
+#ifdef DEBUG
+ #define DEBUG_START Serial.begin(9600)
+ #define DEBUG_PRINT(x)  Serial.print (x)
+ #define DEBUG_PRINTLN(x)  Serial.println (x)
+ #define DEBUG_PRINTSTATE(x) Serial.print("The current State is "); Serial.println (x)
+#else
+ #define DEBUG_START
+ #define DEBUG_PRINT(x)
+ #define DEBUG_PRINTLN(x)
+ #define DEBUG_PRINTSTATE(x)
+#endif
+/** END Debug Section */
+
+#define BumpLength 6
+#define VehicleLength 12
+#define WindowLength 72
+
+enum State {InitState, ExtendArms, RetractArms, MoveForward, BumpExtendArms, 
+            BumpRetractArms, MoveOverBump, FinalExtendArms, FinalRetractArms, SystemStop};
+State currentState;
+long loopTics;
+long initialTics;
+
+/*
+ * For Demo on 3/21: Leds for motors, Micro Sevos, Limit Switches, Wire Encoder
+ */
+
+void setup() {
+  DEBUG_START;
+  currentState = InitState;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  switch (currentState) {
+    case InitState:
+    //initial Tics = tics
+    break;
+    
+    case ExtendArms:
+    //track motor off
+    //arm motor on
+    //loop Tics = tics
+    break;
+    
+    case RetractArms:
+    //arm motor on reverse
+    break;
+    
+    case MoveForward:
+    //arm motor off
+    //track motor on
+    //lower servo
+    //lower tensioner
+    break;
+    
+    case BumpExtendArms:
+    //track motor off
+    //arm motor on
+    //loop Tics = tics
+    break;
+    
+    case BumpRetractArms:
+    //arm motor on reverse
+    break;
+    
+    case MoveOverBump:
+    //raise servo
+    //raise tensioner
+    //move forward
+    break;
+    
+    case FinalExtendArms:
+    //track motor off
+    //arm motor on
+    break;
+    
+    case FinalRetractArms:
+    //arm motor on reverse
+    break;
+    
+    case SystemStop:
+    //track motor off
+    //arm motor off
+    break;
+    
+    default:
+    //error
+    break;
+  }
+  
+  switch (currentState) {
+    case InitState:
+    DEBUG_PRINTSTATE("InitState");
+    currentState = ExtendArms;
+    DEBUG_PRINTSTATE("ExtendArms");
+    break;
+    
+    case ExtendArms:
+    //If Arm outer limit
+    currentState = RetractArms;
+    DEBUG_PRINTSTATE("RetractArms");
+    break;
+    
+    case RetractArms:
+    //If Arm inner limit
+    currentState = MoveForward;
+    DEBUG_PRINTSTATE("MoveForward");
+    break;
+    
+    case MoveForward:
+    //If Bump
+    currentState = BumpExtendArms;
+    DEBUG_PRINTSTATE("BumpExtendArms");
 
+    //If tics - loopTics >= Vehicle Length
+    currentState = ExtendArms;
+    DEBUG_PRINTSTATE("ExtendArms");
+
+    //If tics - loopTics >= Window Length
+    currentState = FinalExtendArms;
+    DEBUG_PRINTSTATE("FinalExtendArms");
+    break;
+    
+    case BumpExtendArms:
+    //If Arm outer limit
+    currentState = BumpRetractArms;
+    DEBUG_PRINTSTATE("BumpRetractArms");
+    break;
+    
+    case BumpRetractArms:
+    //If Arm inner limit
+    currentState = MoveOverBump;
+    DEBUG_PRINTSTATE("MoveOverBump");
+    break;
+    
+    case MoveOverBump:
+    //If tics - loopTics >= Vehicle Length + Bump Length
+    currentState = ExtendArms;
+    DEBUG_PRINTSTATE("ExtendArms");
+    break;
+    
+    case FinalExtendArms:
+    //If Arm outer limit
+    currentState = FinalRetractArms;
+    DEBUG_PRINTSTATE("FinalRetractArms");
+    break;
+    
+    case FinalRetractArms:
+    //If Arm inner limit
+    currentState = SystemStop;
+    DEBUG_PRINTSTATE("SystemStop");
+    break;
+    
+    case SystemStop:
+    break;
+    
+    default:
+    //error
+    break;
+  }
 }
